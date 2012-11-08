42dd3f62e2f7a6c94938973a848bd6bf51a15e5b
Dmitry Bobrov <me@dbobrov.com>
2012-11-08 16:54:08 +0400

    deduction, parser works

--- second_homework/Deduction/main.ml
+++
@@ -1,8 +0,0 @@
-type expression = 
-	| PropositionalVariable of string
-	| Implication of expression * expression
-	| Conjunction of expression * expression
-	| Disjunction of expression * expression
-	| Negation of expression 
-	;;
-
--- second_homework/Deduction/main.mli
+++ second_homework/Deduction/main.mli
@@ -1,6 +1,1 @@
-type expression =
-    PropositionalVariable of string
-  | Implication of expression * expression
-  | Conjunction of expression * expression
-  | Disjunction of expression * expression
-  | Negation of expression
+
