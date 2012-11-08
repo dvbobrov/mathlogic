bb0a58f574dec325788f11e47cab98e30e209eeb
Dmitry Bobrov <me@dbobrov.com>
2012-11-08 15:22:45 +0400

    Deduction initial commit

---
+++ second_homework/Deduction/main.mli
@@ -0,0 +1,6 @@
+type expression =
+    PropositionalVariable of string
+  | Implication of expression * expression
+  | Conjunction of expression * expression
+  | Disjunction of expression * expression
+  | Negation of expression
